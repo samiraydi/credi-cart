03f5754885da84a4c1c5bab7cbbe07d4ebfc89f1
samiraydi <samiraydi26@gmail.com>
2019-07-04 11:06:31 +0100

    Updates

---
+++ precache-manifest.df7cb662d366bf0f58852be24c8ce279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d86fd8b8c8ab2742cfe36be77ee1c7",
+    "url": "/credi-cart/index.html"
+  },
+  {
+    "revision": "5a5c4d91fc352c2a71d6",
+    "url": "/credi-cart/static/css/main.fe8cf8b0.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/credi-cart/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "5a5c4d91fc352c2a71d6",
+    "url": "/credi-cart/static/js/main.2c034aff.chunk.js"
+  },
+  {
+    "revision": "e2e4c5185edf2de2f742",
+    "url": "/credi-cart/static/js/runtime~main.29e9f327.js"
+  },
+  {
+    "revision": "524be8a1df870922803edc6c777f8d93",
+    "url": "/credi-cart/static/media/img1.524be8a1.jpg"
+  },
+  {
+    "revision": "e090a7339a7c1fff996351523ebbcdc7",
+    "url": "/credi-cart/static/media/visa1.e090a733.png"
+  }
+]);
